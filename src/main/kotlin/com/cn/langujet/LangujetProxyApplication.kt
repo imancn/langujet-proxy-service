f79b55bd39a57886dc0b5a9f029bcc9ae16b23df
Iman Soltani <iman.soltani.j@gmail.com>
2024-12-24 10:54:06 +0330

    Initialize

---
+++ src/main/kotlin/com/cn/langujet/LangujetProxyApplication.kt
@@ -0,0 +1,13 @@
+package com.cn.langujet
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+import org.springframework.cloud.openfeign.EnableFeignClients
+
+@SpringBootApplication
+@EnableFeignClients
+class LangujetProxyApplication
+
+fun main(args: Array<String>) {
+    runApplication<LangujetProxyApplication>(*args)
+}
