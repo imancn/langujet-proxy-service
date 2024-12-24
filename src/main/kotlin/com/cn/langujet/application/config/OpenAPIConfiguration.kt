7e47c1da91a98575eae56852822d383659bd9dd4
Iman Soltani <iman.soltani.j@gmail.com>
2024-12-24 10:56:42 +0330

    Config Swagger

---
+++ src/main/kotlin/com/cn/langujet/application/config/OpenAPIConfiguration.kt
@@ -0,0 +1,35 @@
+package com.cn.langujet.application.config
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType
+import io.swagger.v3.oas.annotations.info.Contact
+import io.swagger.v3.oas.annotations.info.Info
+import io.swagger.v3.oas.annotations.info.License
+import io.swagger.v3.oas.annotations.security.SecurityRequirement
+import io.swagger.v3.oas.annotations.security.SecurityScheme
+import io.swagger.v3.oas.annotations.servers.Server
+
+@OpenAPIDefinition(
+    info = Info(
+        title = "Langujet",
+        description = "API documentation",
+        contact = Contact(
+            name = "Langujet",
+            url = "https://langujet.com",
+            email = "info@langujet.com"
+        ),
+        license = License(
+            name = "All rights reserved for Langujet Co.",
+            url = "https://langujet.com")
+    ),
+    servers = [Server(url = "/")],
+    security = [SecurityRequirement(name = "bearerAuth")]
+)
+@SecurityScheme(
+    name = "bearerAuth",
+    type = SecuritySchemeType.APIKEY,
+    `in` = SecuritySchemeIn.HEADER,
+    paramName = "Authorization"
+)
+class OpenAPIConfiguration
